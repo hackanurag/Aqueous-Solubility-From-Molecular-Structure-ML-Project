1761e7fddaed4a5a5b8c31e90d6b907f27233495
hackanurag <anurag.datta.a@gmail.com>
2024-04-26 23:38:08 +0530

    X matrix is completed for ML model

--- python_codebase/predicting_solubility.py
+++ python_codebase/predicting_solubility.py
@@ -1,6 +1,9 @@
 import sys
 import pandas as pd
 import requests
+from rdkit import Chem
+import numpy as np
+from rdkit.Chem import Descriptors
 
 try:
     r = requests.get('https://raw.githubusercontent.com/dataprofessor/data/master/delaney.csv')
@@ -8,3 +11,52 @@ try:
         sol = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/delaney.csv')
 except Exception as e:
     print (e)
+
+mol_list= []
+for element in sol.SMILES:
+  mol = Chem.MolFromSmiles(element)
+  mol_list.append(mol)
+
+def generate(smiles):
+    moldata= []
+    for elem in smiles:
+        mol=Chem.MolFromSmiles(elem) 
+        moldata.append(mol)
+       
+    baseData= np.arange(1,1)
+    i=0  
+    for mol in moldata:        
+        desc_MolLogP = Descriptors.MolLogP(mol)
+        desc_MolWt = Descriptors.MolWt(mol)
+        desc_NumRotatableBonds = Descriptors.NumRotatableBonds(mol)
+      
+        row = np.array([desc_MolLogP, desc_MolWt, desc_NumRotatableBonds])   
+    
+        if(i==0):
+            baseData=row
+        else:
+            baseData=np.vstack([baseData, row])
+        i=i+1      
+    
+    columnNames=["MolLogP","MolWt","NumRotatableBonds"]   
+    descriptors = pd.DataFrame(data=baseData,columns=columnNames)
+    
+    return descriptors
+
+df = generate(sol.SMILES)
+
+def AromaticAtoms(m):
+  aromatic_atoms = [m.GetAtomWithIdx(i).GetIsAromatic() for i in range(m.GetNumAtoms())]
+  aa_count = []
+  for i in aromatic_atoms:
+    if i==True:
+      aa_count.append(1)
+  sum_aa_count = sum(aa_count)
+  return sum_aa_count
+
+desc_AromaticProportion = [AromaticAtoms(element)/Descriptors.HeavyAtomCount(element) for element in mol_list]
+df_desc_AromaticProportion = pd.DataFrame(desc_AromaticProportion, columns=['AromaticProportion'])
+
+X = pd.concat([df,df_desc_AromaticProportion], axis=1)
+
+print(X)
